81211d7268b95ae6c004d04d2c5fd55530d326a6
Index_404 <63930715+youtube-dm94@users.noreply.github.com>
2021-10-22 06:05:41 +0800

    Chapter 2 ch02_3
    FunctionComponent
    ScrollView

--- ch02/ch02_3/App.tsx
+++ ch02/ch02_3/App.tsx
@@ -1,13 +1,35 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
-import ClassComponent from "./src/screens/ClassComponent";
-import ArrowComponent from "./src/screens/ArrowComponent";
+// import React from "react";
+// import { SafeAreaView } from "react-native";
+// import ClassComponent from "./src/screens/ClassComponent";
+// import ArrowComponent from "./src/screens/ArrowComponent";
+// import Person from "./src/screens/Person";
+// import * as D from './src/data'
 
-export default function App(){
+// const person = D.createRandomPerson()
+// export default function App(){
+//   return (
+//     <SafeAreaView>
+//       <ClassComponent/>
+//       <ArrowComponent/>
+//       <Person person={person} />
+//     </SafeAreaView>
+//   )
+// }
+
+// =======  Adding ScrollViw =======
+import React from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import Person from './src/screens/Person'
+import * as D from './src/data'
+
+const people = D.makeArray(100).map(D.createRandomPerson)
+export default function App() {
+  const children = people.map((person) => (
+    <Person key={person.id} person={person} />    //Each child in a list has a unique key
+  ))
   return (
     <SafeAreaView>
-      <ClassComponent/>
-      <ArrowComponent/>
+      <ScrollView>{children}</ScrollView>
     </SafeAreaView>
   )
 }
