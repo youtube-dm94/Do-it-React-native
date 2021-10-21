892171b973b90dbdc343fc0d5c3be67489e97119
Index_404 <63930715+youtube-dm94@users.noreply.github.com>
2021-10-22 06:42:04 +0800

    Chapter 2 cd02_4
    Event Property

--- ch02/ch02_4/App.tsx
+++ ch02/ch02_4/App.tsx
@@ -1,11 +1,35 @@
-import React, { Children } from 'react'
+import React from 'react'
 import { SafeAreaView, Alert, Button } from 'react-native'
+import { TouchableOpacity, TouchableHighlight, Text } from 'react-native'
+import { TextInput } from 'react-native'
+
+const onPress = () => Alert.alert('home pressed.', 'message')
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Button title = 'Home'
-        onPress={() => Alert.alert('home pressed.', 'message')} />
+      <Button title = 'Home' onPress={onPress} />
+      <TouchableOpacity onPress={onPress}>
+        <Text>TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight onPress={onPress}>
+        <Text>TouchableHighlight</Text>
+      </TouchableHighlight>
+      <TextInput
+        placeholder="enter your name"
+        onChangeText={(text: string) => console.log(text)}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onEndEditing={() => console.log('onEndEditing')}
+      />
+      <TextInput
+        placeholder="enter your email"
+        onChangeText={(text: string) => console.log(text)}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onEndEditing={() => console.log('onEndEditing')}
+        keyboardType="email-address"
+      />
     </SafeAreaView>
   )
 }
