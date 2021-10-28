132f8b4c3917216821a786ca18d0342890b1cb38
Index_404 <63930715+youtube-dm94@users.noreply.github.com>
2021-10-29 03:50:39 +0800

    Chapter 3 ch03_1
    Package used:
    react-native-paper
    color

--- ch03/ch03_1/App.tsx
+++ ch03/ch03_1/App.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { StyleSheet,SafeAreaView, Text } from "react-native";
+import {Colors} from 'react-native-paper'
+import Color from 'color'
 
+console.log(Colors.blue500)  // #2196f3
+console.log(Color(Colors.blue500).alpha(0.5).lighten(0.5).string()) // hsla((206, 89.7%, 81.2%, 0.5))
 export default function App(){
   return(
-    <SafeAreaView style={[styles.safeAreaView, {backgroundColor: 'blue'}]}>
-      <Text style={[styles.text,{color:'white'}]}>
+    <SafeAreaView style={[styles.safeAreaView]}>
+      <Text style={[styles.text]}>
         Hello StyleSheet World!
       </Text>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
-  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center'},
-  text: {fontSize: 20}
+  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center',
+    backgroundColor: Colors.blue500},
+  text: {fontSize: 20, color:Color(Colors.blue500).alpha(0.7).lighten(0.9).string()}
 })
