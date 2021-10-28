511b6d5e5a963d4f663e8a76b6f57e09a1a85339
Index_404 <63930715+youtube-dm94@users.noreply.github.com>
2021-10-29 04:39:07 +0800

    Chapter 3 ch03_2
    Update on App.tsx
    border styles
    Platform.select

--- ch03/ch03_2/App.tsx
+++ ch03/ch03_2/App.tsx
@@ -2,9 +2,7 @@ import React from "react";
 // prettier-ignore
 import { Platform, Dimensions, StyleSheet, SafeAreaView, Text, View } from 'react-native'
 import {Colors} from 'react-native-paper'
-import color from 'color'
-
-const {width, height} = Dimensions.get('window')
+let {width, height} = Dimensions.get('window')
 
 export default function App(){
   return(
@@ -12,18 +10,19 @@ export default function App(){
       <Text style={[styles.text]}> os: {Platform.OS} </Text>
       <Text style={[styles.text]}> width: {width} px </Text>
       <Text style={[styles.text]}> height: {height} px </Text>
-
+      <View style={[styles.box, {borderRadius: 10}]} />
       <View style={[styles.box, styles.border]} />
-      <View style={[styles.box, styles.border, {borderRadius: 20}]} />
-      <View style={[styles.box, styles.border,
-        {borderTopLeftRadius: 40, borderBottomLeftRadius: 40}]} />
+      <View style={[styles.box, styles.border, {borderRadius: 10}]} />
     </SafeAreaView>
   )
 }
 // prettier-ignore
 const styles = StyleSheet.create({
-  safeAreaView: {backgroundColor: Colors.blue500, flex: 1, padding: 10}, // set background to whole screen, height:height or '100%'
-  text: {marginBottom: 10, fontSize: 20, color: color(Colors.blue500).lighten(0.9).string()},
-  box: { height: 100, backgroundColor: Colors.lime500, marginBottom: 10},
-  border: {borderWidth: 10, borderColor: color('black').alpha(0.3).string() }
+  safeAreaView: {backgroundColor: Colors.blue500, flex: 1, 
+    paddingLeft: Platform.select({ios: 0, android: 20})}, // set background to whole screen, height:height or '100%'
+  text: {marginBottom: 10, fontSize: 20, marginLeft: 50, marginTop: 20},
+  box: { width: '70%', height: 100, backgroundColor: 'white', marginBottom: 10,
+      marginLeft: Platform.select({ios:20, android: 0})  
+  },
+  border: {borderWidth: 10, borderColor: Colors.lime500 }
 })
