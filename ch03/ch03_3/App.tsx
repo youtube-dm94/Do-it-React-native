22faa3a2515d798b4b2c45d3ea16bab17063d0d0
Index_404 <63930715+youtube-dm94@users.noreply.github.com>
2021-11-12 02:46:58 +0800

    Chapter 3 ch03_3
    Component Styling
    react-native-paper
    ImageBackground
    npx react-native link
    fontFamily
    react-native-vector-icons

--- ch03/ch03_3/App.tsx
+++ ch03/ch03_3/App.tsx
@@ -1,10 +1,43 @@
 import React from "react";
 //prettier-ignore
-import {StyleSheet, SafeAreaView} from 'react-native'
+import {StyleSheet, Platform, SafeAreaView, ImageBackground, Image, View, Text} from 'react-native'
+import * as D from './src/data'
+
+const avatarUrl = D.randomAvatarUrl()
+const avatarSize = 50
+
+const text = 'Almost before we knew it, we had left the ground.'
 
 export default function App(){
-  return<SafeAreaView style={styles.flex} />
+  return(
+    <SafeAreaView style={[styles.flex]}>
+      <ImageBackground
+        style={[styles.backgroundImage]}
+        source={require('./src/assets/images/bg.jpg')}>
+        <Image source={{uri: avatarUrl}} style={[styles.image]}/>
+        <View style={[styles.flex, styles.padding10]}>
+          <Text style={[styles.text, styles.regular]}>{text} [regular]</Text>
+          <Text style={[styles.text, styles.medium]}>{text} [medium]</Text>
+          <Text style={[styles.text, styles.semiBold]}>{text} [semi bold]</Text>
+          <Text style={[styles.text, styles.bold]}>{text} [bold]</Text>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  )
 }
+// prettier-ignore
 const styles = StyleSheet.create({
-  flex: {flex: 1}
+  flex: {flex: 1},
+  backgroundImage: {flex: 1},
+  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2},
+  padding10: {padding: 10},
+  text: {textAlign: 'center', fontSize: 25, color: 'white', marginBottom: 10},
+
+  regular: {fontFamily: 'Genos-Regular', fontWeight: '400'},
+  medium: {fontFamily: 'Genos-Medium', fontWeight: '500'},
+  semiBold: {fontFamily: 'Genos-SemiBold', fontWeight: '600'},
+  bold: {
+    fontFamily: 'Genos-Bold', 
+    fontWeight: Platform.select({ios: '700', android: '600'})
+  }
 })
