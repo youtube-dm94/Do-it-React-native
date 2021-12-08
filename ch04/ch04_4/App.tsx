18d1753cb6078ab1748be0df979691d827cca3a2
Index_404 <63930715+youtube-dm94@users.noreply.github.com>
2021-12-09 05:35:02 +0800

    Chapter4 ch04_4
    useEffect, useLayoutEffect hook

--- ch04/ch04_4/App.tsx
+++ ch04/ch04_4/App.tsx
@@ -1,115 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, {useState, useMemo, useCallback} from 'react'
+import {StyleSheet, SafeAreaView, View, Text} from 'react-native'
+import {Colors} from 'react-native-paper'
+import LifeCycle from './src/screens/LifeCycle'
+import Timer from './src/screens/Timer'
+import Interval from './src/screens/Interval'
+import Fetch from './src/screens/Fetch'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+// prettier-ignore
+export default function App() {
+  const selects = useMemo(() => ['lifeCycle', 'timer', 'interval', 'fetch'], [])
+  const [select, setSelect] = useState<string>(selects[0])
+  const onPress = useCallback((text) => () => setSelect(text), [])
+  const buttons = useMemo(() => 
+    selects.map((text) => (
+      <Text key={text} onPress={onPress(text)} style={styles.button}>
+        {text}
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    )), [])
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.topBar}>{buttons}</View>
+      {select == 'lifeCycle' && <LifeCycle />}
+      {select == 'timer' && <Timer />}
+      {select == 'interval' && <Interval />}
+      {select == 'fetch' && <Fetch />}
     </SafeAreaView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  safeAreaView: {flex: 1},
+  topBar: {flexDirection: 'row', flexWrap: 'wrap', padding: 5,
+    justifyContent: 'space-between', backgroundColor: Colors.lightBlue500},
+  button: {fontSize: 20, color: 'white'}
+})
---
+++ ch04/ch04_4/src/screens/Fetch.tsx
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from "react";
+import {StyleSheet, View, Text, FlatList} from 'react-native'
+import { Colors } from "react-native-paper";
+import Country from './Country'
+import * as D from '../data'
+
+// prettier-ignore
+const title = 'CopyMe'
+export default function Fetch() {
+    const [countries, setCountries] = useState<D.ICountry[]>([])
+    const [error, setError] = useState<Error | null>(null)
+    useEffect(() => {
+        D.getCountries().then(setCountries).catch(setError)
+    }, [])
+    return(
+        <View style={[styles.view]}>
+            <Text style={[styles.title]}>Fetch</Text>
+            {error && <Text>{error.message}</Text>}
+            <FlatList data={countries} showsVerticalScrollIndicator={false}
+                renderItem={({item}) => <Country country={item} />}
+                keyExtractor={(item, index) => index.toString()}
+                ItemSeparatorComponent={() => <View style={styles.separator} />} />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    view: {flex: 1, padding: 5, backgroundColor: Colors.blue900},
+    title: {fontSize: 30, fontWeight: '600'},
+    separator: {borderBottomColor: Colors.blue50, borderBottomWidth: 1}
+})
